0b2f5b9c27bde1eb0fc55e43c7d0e6697e437d4f
Krzysztof Rećko <yetioszek@gmail.com>
2016-04-02 09:54:24 +0200

    Update util.h

--- pd2/main.cu
+++ pd2/main.cu
@@ -1,18 +1,21 @@
 #include "reverse.cuh"
+#include "util.h"
 
 #define N 1000
 
 int main()
 {
 	srand(time(NULL));
-
-	int *tab;
-	int *devTab;
-
-	cudaMallocHost((void**)&tab, sizeof(tab[0]) * N);
-	cudaMalloc((void**)&devTab, sizeof(devTab[0]) * N);
+    SyncMemory<int> tab(N);
 
 	for (int i = 0; i < N; ++i) {
-		tab[i] = rand();
+		tab.getHost()[i] = rand();
 	}
+
+    tab.syncToDevice();
+
+    Timer t;
+    t.startTimer();
+    reverse<int>(tab.getDevice(), N);
+    std::cout << t.stopTimer() << std::endl;
 }
---
+++ util.h
@@ -0,0 +1,49 @@
+#define CUDA_CHECK_RETURN(value) { \
+cudaError_t _m_cudaStat = value; \
+if (_m_cudaStat != cudaSuccess) { \
+	fprintf(stderr, "Error %s at line %d in file %s\n", \
+	cudaGetErrorString(_m_cudaStat), __LINE__, __FILE__); \
+	exit(1); \
+} }
+
+class Timer {
+	timeval start;
+public:
+	void startTimer() {
+		gettimeofday(&start, 0);
+	}
+
+	float stopTimer() {
+		timeval end;
+		gettimeofday(&end, 0);
+		float sec = end.tv_sec - start.tv_sec;
+		float usec = end.tv_usec - start.tv_usec;
+		return sec + (usec / 1000000.0);
+	}
+};
+
+template <typename T>
+class SyncMemory {
+    void *host, *device;
+    size_t size;
+public:
+    SyncMemory(int n) {
+        size = sizeof(T) * n;
+        cudaMallocHost((void**) &host, size);
+        cudaMalloc((void**) &device, size);
+    }
+    ~SyncMemory() {
+        cudaFreeHost((void*) host);
+        cudaFree((void*) device);
+    }
+
+    void syncToDevice() {
+        cudaMemcpy(host, device, size, cudaMemcpyHostToDevice);
+    }
+    void syncToHost() {
+        cudaMemcpy(device, host, size, cudaMemcpyDeviceToHost);
+    }
+
+    T *getHost() { return host; }
+    T *getDevice() { return device; }
+}
