4a716f5282b258e9e7249fc04c986b57fbfa3413
Krzysztof Recko <yetioszek@gmail.com>
2016-06-08 00:50:48 +0200

    Experimental only count ig12

--- duze/test/test.sh
+++ duze/test/test.sh
@@ -4,13 +4,16 @@
 
 rm result_c.out
 rm result_c2.out
+rm result_c3.out
 
 function tess {
 	echo "Testing $1"
 	#printf "\n========Testing $1=========\n\n" >> result_c.out
-	printf "\n========Testing $1=========\n\n" >> result_c2.out
 	#../gpu_main_c < autotest_$1.in > autotest_$1_c_gpu.out 2>> result_c.out
-	../gpu_main_c2 < autotest_$1.in > autotest_$1_c2_gpu.out 2>> result_c2.out
+	#printf "\n========Testing $1=========\n\n" >> result_c2.out
+	#../gpu_main_c2 < autotest_$1.in > autotest_$1_c2_gpu.out 2>> result_c2.out
+	printf "\n========Testing $1=========\n\n" >> result_c3.out
+	../gpu_main_c3 < autotest_$1.in > autotest_$1_c3_gpu.out 2>> result_c3.out
 	#diff autotest_$1_c_gpu.out autotest_$1_c2_gpu.out >> result_c2.out
 }
 
@@ -21,9 +24,9 @@ tess 04
 tess 04a
 tess 05
 tess 04b
-tess 05a
+#tess 05a
 tess 03a
-tess 05b
+#tess 05b
 #tess 04c
 #tess 06a
 #tess 06
