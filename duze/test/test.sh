e5865099e61295f35f0ff2dcd956e5777fddaa26
Krzysztof Recko <yetioszek@gmail.com>
2016-06-07 18:14:01 +0200

    linux shell script

--- duze/test/test.sh
+++ duze/test/test.sh
@@ -4,18 +4,44 @@
 
 rm result_c.out
 
-../gpu_main_c < autotest_01.in > autotest_01_c_gpu.out 2>> result_c.out
-../gpu_main_c < autotest_02.in > autotest_02_c_gpu.out 2>> result_c.out
-../gpu_main_c < autotest_03.in > autotest_03_c_gpu.out 2>> result_c.out
-../gpu_main_c < autotest_03a.in > autotest_03a_c_gpu.out 2>> result_c.out
-../gpu_main_c < autotest_04.in > autotest_04_c_gpu.out 2>> result_c.out
-../gpu_main_c < autotest_04a.in > autotest_04a_c_gpu.out 2>> result_c.out
-../gpu_main_c < autotest_04b.in > autotest_04b_c_gpu.out 2>> result_c.out
-../gpu_main_c < autotest_04c.in > autotest_04c_c_gpu.out 2>> result_c.out
-../gpu_main_c < autotest_05.in > autotest_05_c_gpu.out 2>> result_c.out
-../gpu_main_c < autotest_05a.in > autotest_05a_c_gpu.out 2>> result_c.out
-../gpu_main_c < autotest_05b.in > autotest_05b_c_gpu.out 2>> result_c.out
-../gpu_main_c < autotest_06.in > autotest_06_c_gpu.out 2>> result_c.out
-../gpu_main_c < autotest_06a.in > autotest_06a_c_gpu.out 2>> result_c.out
-../gpu_main_c < autotest_07.in > autotest_07_c_gpu.out 2>> result_c.out
-../gpu_main_c < autotest_08.in > autotest_08_c_gpu.out 2>> result_c.out
+function tess {
+	echo "Testing $1"
+	../gpu_main_c < autotest_$1.in > autotest_$1_c_gpu.out 2>> result_c.out
+}
+
+tess 01
+tess 02
+tess 03
+tess 03a
+tess 04
+tess 04a
+tess 04b
+tess 04c
+tess 05
+tess 05a
+tess 05b
+tess 06
+tess 06a
+tess 07
+tess 08
+
+#echo "Testing test 01"
+#../gpu_main_c < autotest_01.in > autotest_01_c_gpu.out 2>> result_c.out
+#echo "Testing test 01"
+#../gpu_main_c < autotest_02.in > autotest_02_c_gpu.out 2>> result_c.out
+#echo "Testing test 01"
+#../gpu_main_c < autotest_03.in > autotest_03_c_gpu.out 2>> result_c.out
+#echo "Testing test 01"
+#../gpu_main_c < autotest_03a.in > autotest_03a_c_gpu.out 2>> result_c.out
+#echo "Testing test 01"
+#../gpu_main_c < autotest_04.in > autotest_04_c_gpu.out 2>> result_c.out
+#../gpu_main_c < autotest_04a.in > autotest_04a_c_gpu.out 2>> result_c.out
+#../gpu_main_c < autotest_04b.in > autotest_04b_c_gpu.out 2>> result_c.out
+#../gpu_main_c < autotest_04c.in > autotest_04c_c_gpu.out 2>> result_c.out
+#../gpu_main_c < autotest_05.in > autotest_05_c_gpu.out 2>> result_c.out
+#../gpu_main_c < autotest_05a.in > autotest_05a_c_gpu.out 2>> result_c.out
+#../gpu_main_c < autotest_05b.in > autotest_05b_c_gpu.out 2>> result_c.out
+#../gpu_main_c < autotest_06.in > autotest_06_c_gpu.out 2>> result_c.out
+#../gpu_main_c < autotest_06a.in > autotest_06a_c_gpu.out 2>> result_c.out
+#../gpu_main_c < autotest_07.in > autotest_07_c_gpu.out 2>> result_c.out
+#../gpu_main_c < autotest_08.in > autotest_08_c_gpu.out 2>> result_c.out
