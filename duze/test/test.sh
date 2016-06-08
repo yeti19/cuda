8aa8359a4a93e7683434d3611db4a4bd5c6bd2e3
Krzysztof Recko <yetioszek@gmail.com>
2016-06-08 11:24:47 +0200

    Next testing with unrolling

--- duze/test/test.sh
+++ duze/test/test.sh
@@ -15,8 +15,12 @@ function tess {
 	../gpu_main_c2 < autotest_$1.in > autotest_$1_c2_gpu.out 2>> result_c2.out
 	printf "\n========Testing $1=========\n\n" >> result_c3.out
 	../gpu_main_c3 < autotest_$1.in > autotest_$1_c3_gpu.out 2>> result_c3.out
+	printf "\n========Testing $1=========\n\n" >> result_c4.out
+	../gpu_main_c4 < autotest_$1.in > autotest_$1_c4_gpu.out 2>> result_c4.out
+	printf "\n========Testing $1=========\n\n" >> result_c5.out
+	../gpu_main_c5 < autotest_$1.in > autotest_$1_c5_gpu.out 2>> result_c5.out
 	printf "\n========Testing $1=========\n\n" >> result_d.out
-	../gpu_main_c3 < autotest_$1.in > autotest_$1_d_gpu.out 2>> result_d.out
+	../gpu_main_d < autotest_$1.in > autotest_$1_d_gpu.out 2>> result_d.out
 	#diff autotest_$1_c_gpu.out autotest_$1_c2_gpu.out >> result_c2.out
 }
 
@@ -25,10 +29,10 @@ function tess {
 #tess 03
 #tess 04
 tess 04a
-tess 05
+#tess 05
 #tess 04b
 #tess 05a
-#tess 03a
+tess 03a
 #tess 05b
 #tess 04c
 #tess 06a
